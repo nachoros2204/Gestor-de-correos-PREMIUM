d558da6785b1aca26bde51dba375d034b452b45e
Nacho Rosales <nacho.rosales04@gmail.com>
2024-10-01 15:25:57 -0300

    Agregue filtro.java en filtros

---
+++ Filtros/Filtro.java
@@ -0,0 +1,26 @@
+public class Filtro implements INameable {
+    private String nombre = "";
+
+    protected ArrayList<Correo> buscarString(String buscar, Buzon buzon) {
+        return null;
+    }
+}
+
+public ArrayList<Correo> filtrar (String buscarTitulo, String buscarOtro, Buzon buzon){
+    buscarString(buscarTitulo, buzon);
+    buscarString(buscarOtro, buzon);
+    return null;
+}
+
+public ArrayList<Correo> filtrar (String buscar, Buzon buzon){
+    ArrayList<Correo> correos = buscarString(buscar, buzon);
+    return correos;
+}
+
+public void setNombre (String nombre){
+    this.nombre = nombre;
+}
+
+public String getNombre(){
+    return nombre;
+}
