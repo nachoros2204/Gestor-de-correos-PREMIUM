03f89673ceff56254a390aedda5a97b115cafd9c
lourdesgomezsierra <lourdesmariagomezsierra@gmail.com>
2024-10-01 13:51:13 -0300

    cambios mail.java

--- Mail.java
+++ Mail.java
@@ -1,1 +1,15 @@
-hola como estas
+import java.util.ArrayList;
+
+public class Mail {
+    private String titulo;
+    private String mensaje;
+    private String remitente;
+    private ArrayList<String> destinatario = new ArrayList<>();
+    
+    public Mail(String titulo, String mensaje, String remitente, ArrayList<String> destinatario){
+        this.titulo = titulo;
+        this.mensaje = mensaje;
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+    }
+}
