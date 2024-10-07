69921ad994fcc7993ced6ae11ab559d555a8d9b4
JuaniGualtieri <juanigualtierics@gmail.com>
2024-10-07 19:18:51 -0300

    constructor de usuario y métodos relacionados con persona

--- main/Usuario.java
+++ main/Usuario.java
@@ -16,3 +16,33 @@ public class Usuario implements INombre, IApellido, IDireccion {
     private Persona persona = new Persona("", "", "");
 
     }
+
+    public Usuario (String nombre, String apellido, String direccionCorreo){
+        persona.setNombre(nombre);
+        persona.setApellido(apellido);
+        persona.setDireccionCorreo(direccionCorreo);
+    }
+
+    public void setDireccionCorreo (String direccionCorreo){
+        persona.setDireccionCorreo(direccionCorreo);
+    }
+
+    public String getDireccionCorreo(){
+        return persona.getDireccionCorreo();
+    }
+
+    public void setNombre (String nombre){
+        persona.setNombre(nombre);
+    } 
+
+    public String getNombre() {
+        return persona.getNombre();
+    }
+
+    public void setApellido(String apellido) {
+        persona.setApellido(apellido);
+    }
+
+    public String getApellido() {
+        return persona.getApellido();
+    }
