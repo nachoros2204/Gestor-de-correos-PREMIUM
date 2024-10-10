ae9f3d3b5c0d346db7d61a9454cf1bf5e18516fc
JuaniGualtieri <juanigualtierics@gmail.com>
2024-10-10 09:57:33 -0300

    correcion de FiltroDeTitulo

--- main/Filtros/FiltroDeTitulo.java
+++ main/Filtros/FiltroDeTitulo.java
@@ -14,7 +14,7 @@ public class FiltroDeTitulo extends Filtro {
      protected ArrayList<Mail> buscarString(String buscar, ArrayList<Mail> correos) {
        // Filtra los correos que tienen el remitente que contiene el texto buscado
         return correos.stream()
-        .filter(correo -> correo.getRemitente().contains(buscar))
+        .filter(correo -> correo.getTitulo().contains(buscar))
         .collect(Collectors.toCollection(ArrayList::new)); // Colecciona los correos filtrados en un ArrayList
 }
 }
