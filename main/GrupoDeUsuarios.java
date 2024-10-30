d4722ff803028b999b8d32e2c57dceab4a6b89e2
lourdesgomezsierra <lourdesmariagomezsierra@gmail.com>
2024-10-30 15:50:09 -0300

    grupos

--- main/GrupoDeUsuarios.java
+++ main/GrupoDeUsuarios.java
@@ -8,7 +8,7 @@ public class GrupoDeUsuarios {
     public GrupoDeUsuarios(String nombre) {
         this.nombre = nombre;
     }
-    public void agregarUsuarioAlGrupo(Usuario usuario) {
+    public void agregarUsuarioAlGrupo(Usuario usuario) { // verifica si el usuario ya existe en la lista
         if (!miembros.contains(usuario)) {
             miembros.add(usuario);
         }
